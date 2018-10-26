5a127137d6cef2a8ce7689e0108431af3c9e300d
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-10-26 12:49:24 +0200

    Update PasswordHash to use external dependency

--- src/graphql/model.schema.ts
+++ src/graphql/model.schema.ts
@@ -21,28 +21,10 @@ import {
   GraphQLScalarLiteralParser,
   ValueNode,
 } from 'graphql';
-import { sha512 } from 'js-sha512';
 import { GraphQLDateTime } from 'graphql-iso-date';
+import { GraphQLPasswordHash } from 'gql-directives';
 
 import { StoreEvent, getChangedColumns } from '../store/store-event.model';
-
-const GraphQLPasswordHash = new GraphQLScalarType({
-  name: 'PasswordHash',
-  description: 'adsfgsdthsdh',
-  serialize: (value: string) => {
-    return value;
-  },
-  parseValue: (value: string) => {
-    return sha512(value);
-  },
-  parseLiteral: (valueNode: ValueNode) => {
-    if (valueNode.kind !== Kind.STRING) {
-      throw new TypeError(`PasswordHash cannot represent non string type`);
-    } else {
-      return sha512(valueNode.value);
-    }
-  },
-});
 
 const entityInterface = new GraphQLInterfaceType({
   name: 'Entity',
